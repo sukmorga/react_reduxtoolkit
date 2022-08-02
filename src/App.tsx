aff61a55e301f9759fdbffe1bfee23c7d20ac88a
sukmorga <naginks@outlook.com>
2022-08-02 03:12:48 +0200

    toollkit features

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import { is } from 'immer/dist/internal';
+import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/redusers/ActionCreators';
 import { userSlice } from './store/redusers/UserSlice';
 
 function App() {
-    const { count } = useAppSelector(state => state.userReducer);
-    const { increment } = userSlice.actions;
     const dispatch = useAppDispatch();
+    const { users, isLoading, error } = useAppSelector(state => state.userReducer);
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [])
+
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={() => dispatch(increment(10))}>Жми</button>
+            {isLoading && <h1>Идет загрузка...</h1>}
+            {error && <h1>{error}</h1>}
+            {JSON.stringify(users, null, 2)}
         </div>
     );
 }
